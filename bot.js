f5eac2bf642e9ffb332071ccff5b5930590ba666
MintyU <deathcat@outlook.kr>
2020-03-26 21:43:46 +0900

    [add]command avatar

--- bot.js
+++ bot.js
@@ -1,23 +1,41 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-require('dotenv').config();
+require("dotenv").config();
 const token = process.env.token;
 const prefix = process.env.prefix;
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (!msg.content.startsWith(prefix)||msg.author.bot) return;
-  const args = msg.content.slice(prefix.length).split(' ');
+client.on("message", msg => {
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+  const args = msg.content.slice(prefix.length).split(" ");
   const command = args.shift().toLowerCase();
 
   if (command === `ping`) {
-    msg.reply('Pong!');
+    msg.reply("Pong!");
   } else if (command === `server`) {
-    msg.reply(`This server's name is: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`)
-  } 
+    msg.reply(
+      `This server's name is: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`
+    );
+  } else if (command === `avatar`) {
+    if (!msg.mentions.users.size) {
+      return msg.channel.send(
+        `Your avatar: <${msg.author.displayAvatarURL({
+          format: "png",
+          dynamic: true
+        })}>`
+      );
+    }
+    const avatarList = msg.mentions.users.map(user => {
+      return `${user.username}'s avatar: <${user.displayAvatarURL({
+        format: "png",
+        dynamic: true
+      })}>`;
+    });
+    msg.channel.send(avatarList);
+  }
 });
 
 client.login(token);
