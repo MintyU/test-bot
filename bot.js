31d635b9677e1ce5d0a4422a2780efb3e93f3740
MintyU <deathcat@outlook.kr>
2020-03-26 20:28:33 +0900

    #[add] command server

--- bot.js
+++ bot.js
@@ -11,6 +11,8 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === `${prefix}ping`) {
     msg.reply('Pong!');
+  } else if (msg.content === `${prefix}server`) {
+    msg.reply(`This server's name is: ${msg.guild.name}`)
   }
 });
 
